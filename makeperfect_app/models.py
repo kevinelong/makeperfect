8415fb557697eaf6b746adf553f27898e4107db1
Tree Casiano <tree.pdx@gmail.com>
2015-08-10 21:59:09 -0700

    Remove extraneous comments and adjust spacing to PEP 8 standard.

--- makeperfect_app/models.py
+++ makeperfect_app/models.py
@@ -1,19 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 class Song(models.Model):
     user = models.ForeignKey(User)
     song_title = models.CharField(max_length=200)
-    # lyrics
     lyrics = models.TextField(default='ADD LYRICS')
-    # chord progression
     chords = models.TextField(blank=True)
-    # original artist
     artist = models.CharField(max_length=100, blank=True)
-    # key
     key = models.CharField(max_length=10, blank=True)
-    # notes/comments
     notes = models.TextField(blank=True)
 
     def __str__(self): # __unicode__ on Python 2
@@ -26,7 +20,7 @@ class List(models.Model):
     user = models.ForeignKey(User)
     list_name = models.CharField(max_length=200)
 
-    def __str__(self): # __unicode__ on Python 2
+    def __str__(self):  # __unicode__ on Python 2
         return self.list_name
 
     def __unicode__(self): # __
@@ -37,11 +31,10 @@ class ListItem(models.Model):
     song = models.ForeignKey(Song)
     position = models.IntegerField(default=0)
 
-    def __str__(self): # __unicode__ on Python 2
+    def __str__(self):  # __unicode__ on Python 2
         return "LIST: " + str(self.list_name) + "; SONG: " + str(self.song)
 
-    def __unicode__(self): # __
+    def __unicode__(self):  # __
         return "LIST: " + str(self.list_name) + "; SONG: " + str(self.song)
-
 
     user = models.ForeignKey(User)
