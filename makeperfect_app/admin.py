01116616fc8d1456027d928dc850b80355383e5b
Tree Casiano <tree.pdx@gmail.com>
2015-08-12 15:05:47 -0700

    Update imports and registration of models.

--- makeperfect_app/admin.py
+++ makeperfect_app/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 from .models import Song
-from .models import List
-from .models import ListItem
+from .models import Setlist
+from .models import SetlistItem
 
 admin.site.register(Song)
-admin.site.register(List)
-admin.site.register(ListItem)
+admin.site.register(Setlist)
+admin.site.register(SetlistItem)
 
