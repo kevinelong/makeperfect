72604db785f72703d048f65586cc7da0b94cd2a6
kevinelong <S!mpl312C0mplexity>
2015-09-04 10:52:27 -0700

    move to python 27

--- makeperfect27/wsgi.py
+++ makeperfect27/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for makeperfect_site project.
+WSGI config for makeperfect27 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "makeperfect_site.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "makeperfect27.settings")
 
 application = get_wsgi_application()
--- makeperfect_app/migrations/0001_initial.py
+++ makeperfect_app/migrations/0001_initial.py
@@ -2,19 +2,54 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
+            name='Setlist',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('setlist_title', models.CharField(max_length=200)),
+                ('setlist_notes', models.TextField(blank=True)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='SetlistItem',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('position', models.IntegerField(default=0)),
+                ('setlist', models.ForeignKey(to='makeperfect_app.Setlist')),
+            ],
+        ),
+        migrations.CreateModel(
             name='Song',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('song_title', models.CharField(max_length=200)),
+                ('lyrics', models.TextField(blank=True)),
+                ('chords', models.TextField(blank=True)),
+                ('artist', models.CharField(max_length=100, blank=True)),
+                ('key', models.CharField(max_length=25, blank=True)),
+                ('song_notes', models.TextField(blank=True)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
+        ),
+        migrations.AddField(
+            model_name='setlistitem',
+            name='song',
+            field=models.ForeignKey(to='makeperfect_app.Song'),
+        ),
+        migrations.AddField(
+            model_name='setlistitem',
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
         ),
     ]
